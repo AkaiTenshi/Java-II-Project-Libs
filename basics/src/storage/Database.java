a082f0c1a0cb8a17c8957396d45b5855ec58b253
Chris Maikos <maikos69@gmail.com>
2016-06-10 12:17:07 +0300

    gui up

--- basics/src/storage/Database.java
+++ basics/src/storage/Database.java
@@ -15,19 +15,26 @@ public class Database {
 
     private static final String SQL_INSERT_CONNECTIONS = "INSERT INTO CONNECTIONS"
             + " VALUES(?, ?, ?)";
+    
+    private static final String SQL_SEARCH_CITIES = "SELECT * FROM CITIES WHERE"
+            + " STATIONNAME LIKE '%?%'";
+    
+    private static final String SQL_SEARCH_CONNECTIONS = "SELECT * FROM LINKS WHERE"
+            + " FROMPOINTNAME LIKE %?% AND TOPOINTNAME LIKE '%?%'";
 
     //Connection to DB
     public static Connection Connect() throws SQLException, ClassNotFoundException {
         Class.forName("oracle.jdbc.driver.OracleDriver"); //Register the Driver
-        Connection con = DriverManager.getConnection("jdbc:oracle:thin:@//10.10.51.123:1521/orcl:1521", "it21456", "JavaTest"); //Connect to the DB
+        Connection con = DriverManager.getConnection("jdbc:oracle:thin:@//10.100.51.123:1521/orcl", "it21456", "JavaTest"); //Connect to the DB
         return con;
     }
 
     public static void writeCitiesToDB(ArrayList<Location> cities) throws SQLException, DBHasDataException, ClassNotFoundException {
         Connection con = Connect();
         con.setAutoCommit(false);
-        PreparedStatement stmn = con.prepareStatement(SQL_INSERT_CITIES);
+        PreparedStatement pstmn = con.prepareStatement(SQL_INSERT_CITIES);
         // Check if the DB is empty and throw Exception
+        Statement stmn = con.createStatement();
         ResultSet rs = stmn.executeQuery("select * from cities");
 
         if (rs.next()) {
@@ -36,12 +43,12 @@ public class Database {
 
         // Instert all the values from cities array to the DB
         for (int i = 0; i <= cities.size() - 1; i++) {
-            stmn.setInt(1, cities.get(i).getId());
-            stmn.setString(2, cities.get(i).getName());
-            stmn.setDouble(3, cities.get(i).getCoord().getX());
-            stmn.setDouble(4, cities.get(i).getCoord().getY());
-            stmn.setString(5, cities.get(i).getType());
-            stmn.executeUpdate();
+            pstmn.setInt(1, cities.get(i).getId());
+            pstmn.setString(2, cities.get(i).getName());
+            pstmn.setDouble(3, cities.get(i).getCoord().getX());
+            pstmn.setDouble(4, cities.get(i).getCoord().getY());
+            pstmn.setString(5, cities.get(i).getType());
+            pstmn.executeUpdate();
         }
         con.commit();
         con.close();
@@ -50,8 +57,9 @@ public class Database {
     public static void writeConnectionsToDB(ArrayList<DirectLink> links) throws SQLException, DBHasDataException, ClassNotFoundException {
         Connection con = Connect();
         con.setAutoCommit(false);
-        PreparedStatement stmn = con.prepareStatement(SQL_INSERT_CONNECTIONS);
+        PreparedStatement pstmn = con.prepareStatement(SQL_INSERT_CONNECTIONS);
         // Check if DB is empty
+        Statement stmn = con.createStatement();
         ResultSet rs = stmn.executeQuery("select * from links");
         if (rs.next()) {
             throw new DBHasDataException("Database is not empty.");
@@ -59,10 +67,10 @@ public class Database {
 
         // Insert all of the links array values to the DB
         for (int i = 0; i <= links.size() - 1; i++) {
-            stmn.setString(1, links.get(i).getFrom());
-            stmn.setString(2, links.get(i).getTo());
-            stmn.setString(3, links.get(i).getType());
-            stmn.executeUpdate();
+            pstmn.setString(1, links.get(i).getFrom());
+            pstmn.setString(2, links.get(i).getTo());
+            pstmn.setString(3, links.get(i).getType());
+            pstmn.executeUpdate();
         }
         con.commit();
         con.close();
@@ -112,5 +120,19 @@ public class Database {
         System.out.println("DELETING");
         stmn.executeUpdate("DELETE FROM links");
         con.close();
+    }
+    
+    public static ResultSet searchDatabase(String term, String databaseName) throws SQLException, ClassNotFoundException {
+        Connection con = Connect();
+        PreparedStatement pstmn = con.prepareStatement(SQL_SEARCH_CITIES);
+        ResultSet rs;
+        
+        if(databaseName.equals("cities")){
+            
+        }else{
+            
+        }
+        
+        return rs;
     }
 }
